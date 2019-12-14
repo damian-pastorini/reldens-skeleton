9534137ff93e567662f8c91850b06e4d6cd80361
Damian A. Pastorini <damian.pastorini>
2019-12-14 20:10:58 +0100

    - Reldens - Game Test

--- dist/styles.b61e60ae.js
+++ dist/styles.b61e60ae.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45867" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45425" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
