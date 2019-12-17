96cafc241003f047ce4e63860dde3eb835b75368
Damian A. Pastorini <damian.pastorini>
2019-12-17 20:21:49 +0100

    - Reldens - Skeleton

--- dist/custom-game-theme-test.e31bb0bc.js
+++ dist/custom-game-theme-test.e31bb0bc.js
@@ -68565,7 +68565,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62377" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57695" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
