fed88f76cc69881933207f078b3b664d7de99a5a
Damian A. Pastorini <damian.pastorini@gmail.com>
2023-01-09 21:01:45 +0100

    - Reldens - Skeleton - Beta.25

--- index.js
+++ index.js
@@ -2,16 +2,18 @@
  *
  * Reldens - NPM Test
  *
- * Server initialization and events.
+ * ServerPlugin initialization and events.
  * This is a test example file on how the server can be initialized.
  *
  */
 
 // required the module:
 const { ServerManager } = require('reldens/server');
+
 // custom plugin (if you like to implement customizations do it there):
 const { ServerPlugin } = require('./theme/plugins/server-plugin');
 // you can find an example of the server file in node_modules/reldens/theme/packages/server-plugin.js
+
 // create a server instance passing the current root:
 let appServer = new ServerManager({
     projectRoot: __dirname, // we need to pass the server root
@@ -19,13 +21,14 @@ let appServer = new ServerManager({
     customPlugin: ServerPlugin
 });
 console.log('TEST - All these are TEST logs that you can remove from your index file.');
+
 // events debug:
 // appServer.events.debug = 'all'; // or any string containing multiple events keys.
 // setup as you need:
-// eslint-disable-next-line no-unused-vars
 appServer.events.on('reldens.serverConfigFeaturesReady', (props) => {
     console.log('TEST - Events test reldens.serverConfigFeaturesReady success!');
 });
+
 // blocked admin actions test:
 if('1' === process.env.RELDENS_BLOCKED_ADMIN){
     appServer.events.on('reldens.beforeCreateAdminManager', (adminPack, dispatchedEvent) => {
@@ -44,6 +47,7 @@ if('1' === process.env.RELDENS_BLOCKED_ADMIN){
         }
     });
 }
+
 // run the server!
 console.log('TEST - ServerPlugin starting...');
 appServer.start().then(() => {
